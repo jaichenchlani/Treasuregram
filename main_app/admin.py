852f489f52c05b34cbf771554030d7ca8933b6d3
Jai Chenchlani <jaichenchlani@gmail.com>
2019-02-08 02:04:41 -0600

    Pluralsight Try Django Course Completed v0.2

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Treasure
 
 # Register your models here.
+# admin.site.Register(Treasure)
+admin.site.register(Treasure)
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from config import logger
+from .models import Treasure
 
 # Create your views here.
 def index(request):
-    logger.debug("Start..")
-    logger.debug("Request:{0}".format(request))
+    # logger.debug("Start..")
+    # logger.debug("Request:{0}".format(request))
+    treasures = Treasure.objects.all()
     logger.debug(str(render(request,'index.html',{"treasures": treasures})))
     return render(request,'index.html',{"treasures": treasures})
 
-class Treasure:
-    def __init__(self, name, value, material, location):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
+# class Treasure:
+#     def __init__(self, name, value, material, location, img_url):
+#         self.name = name
+#         self.value = value
+#         self.material = material
+#         self.location = location
+#         self.img_url = img_url
     
-treasures = [
-    Treasure("Gold Nugget", 500.00, "gold", "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, "pyrite", "Fool's Falls, CO"),
-    Treasure("Coffee Can", 20.00, "tin", "Acme, CA")
-]
-logger.debug(treasures)
+# treasures = [
+#     Treasure("Gold Nugget", 500.00, "gold", "Curly's Creek, NM","static/images/gold-nugget.jpeg"),
+#     Treasure("Fool's Gold", 0, "pyrite", "Fool's Falls, CO","static/images/fools-gold.jpeg"),
+#     Treasure("Coffee Can", 20.00, "tin", "Acme, CA","static/images/coffee-can.jpeg")
+# ]
+# logger.debug(treasures)
